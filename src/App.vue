e5e24c8a5e1f333332880e1fa5ab54c295b34d55
David DeGraw <david.degraw@gmail.com>
2018-03-05 10:21:22 -0700

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ssn-input></ssn-input>
+  </div>
+</template>
+
+<script>
+import SSNInput from './components/SSNInput'
+
+export default {
+  name: 'App',
+  components: {
+    'ssn-input': SSNInput
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SSNInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SSNInput',
+  data () {
+    return {
+      input: 'hi mom'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
